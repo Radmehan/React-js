effd63d2b273f7f27ce2d8f4a652b179ad02a8c8
Md Rafi <mdradmihanchyrafi@gmail.com>
2022-02-01 12:12:05 +0600

    tutorial 6

---
+++ module1.mjs
@@ -0,0 +1,8 @@
+// import ui from './module2.mjs'
+import dz, {b,c,d} from './module2.mjs'
+
+console.log(dz);
+console.log(b,c,d)
+// console.log(b);
+// console.log(c);
+// console.log(d);
---
+++ module2.mjs
@@ -0,0 +1,10 @@
+const a= "harry";
+const b="Rohan";
+const c ="Akash";
+const d="Priyanka";
+
+// export default b;
+export default a;
+export {b};
+export {c};
+export {d};
